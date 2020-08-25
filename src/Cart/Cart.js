6353d4a784d69659a3f5cef94f851cc14a0866ed
Jacob Dale <jacobscottdale@gmail.com>
2020-08-25 10:49:58 -0400

    Separated FEATURES into own component. Separated CartTotal from Cart. Separated CartTotal.css from Cart.css

--- src/Cart/Cart.js
+++ src/Cart/Cart.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import CartItem from '../CartItem/CartItem';
-import { USCurrencyFormat } from '../CurrencyFormat';
 import './Cart.css';
+import CartTotal from '../CartTotal/CartTotal';
 
 class Cart extends Component {
     render() {
@@ -18,21 +18,13 @@ class Cart extends Component {
             );
           });
 
-          const total = Object.keys(this.props.selected).reduce(
-            (acc, curr) => acc + this.props.selected[curr].cost,
-            0
-          );
+          
 
         return (
         <section className="main__summary">
             <h2>Your cart</h2>
             {summary}
-            <div className="summary__total">
-              <div className="summary__total__label">Total</div>
-              <div className="summary__total__value">
-                {USCurrencyFormat.format(total)}
-              </div>
-            </div>
+            <CartTotal selected={this.props.selected} />
           </section>
         )
     }
--- src/CurrencyFormat.js
+++ src/CurrencyFormat.js
@@ -1,5 +1,3 @@
-
-
 export const USCurrencyFormat = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD'
